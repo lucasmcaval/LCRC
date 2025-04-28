b4b4236a0f3ce794ab323baa04e08c62f05ca32b
Rafael Capozzielli <capozzielli.rafael@gmail.com>
2025-04-28 13:55:17 -0300

    Ex 3 - Remover Dados-ESTAVA DANDO RETURN SEM LISTA

--- funcoes.py
+++ funcoes.py
@@ -15,4 +15,5 @@ def guardar_dado(dados_rolados, dados_guardados, indice):
 def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
     dados_rolados.append(dados_no_estoque[dado_para_remover])
     dados_no_estoque.pop(dado_para_remover)
-    return dados_rolados, dados_no_estoque
+    lista = [dados_rolados, dados_no_estoque]
+    return lista
