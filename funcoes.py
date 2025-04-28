ce79950affa96d927949e64387eccb0fa6c258b5
Rafael Capozzielli <capozzielli.rafael@gmail.com>
2025-04-28 13:51:35 -0300

    Ex 3 - Remover Dado

--- funcoes.py
+++ funcoes.py
@@ -11,3 +11,8 @@ def guardar_dado(dados_rolados, dados_guardados, indice):
     dado = dados_rolados.pop(indice)
     dados_guardados.append(dado)
     return [dados_rolados, dados_guardados] 
+
+def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
+    dados_rolados.append(dados_no_estoque[dado_para_remover])
+    dados_no_estoque.pop(dado_para_remover)
+    return dados_rolados, dados_no_estoque
