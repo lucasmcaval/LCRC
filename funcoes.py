194e147e4a1d8d92959344e89c076f6998482a7e
Rafael Capozzielli <capozzielli.rafael@gmail.com>
2025-04-28 14:45:49 -0300

    Ex 6 - Sequencia Baixa

--- funcoes.py
+++ funcoes.py
@@ -31,3 +31,21 @@ def calcula_pontos_soma(lista):
     for valor in lista:
         pontuacao += valor
     return pontuacao
+
+def calcula_pontos_sequencia_baixa(lista):
+    sequencia = 1
+    maior_sequencia = 1
+    lista = list(set(lista))
+    lista.sort()
+    for i in range(len(lista) - 1):
+        if lista[i + 1] == lista[i] + 1:
+            sequencia += 1
+        else:
+            if sequencia > maior_sequencia:
+                maior_sequencia = sequencia
+            sequencia = 1
+            
+    if sequencia >= 4 or maior_sequencia >= 4:
+        return 15
+    else: 
+        return 0
